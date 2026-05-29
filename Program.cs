603ad100cde2088934e672ad7c1a551b9fd72914
Ramon Dantas Policarpo <ramonpolicarpo36@gmail.com>
2026-05-29 18:42:00 -0300

    Adiciona função para instalar e builda o minecraft, iniciando uma sessão offline

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
 ﻿using CmlLib;
 using CmlLib.Core;
+using CmlLib.Core.Auth;
+using CmlLib.Core.ProcessBuilder;
 class Program
 {
     public static async Task Main()
     {
         var launcher = new MinecraftLauncher();
 
+        var game = launcher.InstallAndBuildProcessAsync(
+            "1.8.9", 
+            new MLaunchOption
+            {
+                Session = MSession.CreateOfflineSession("Ramon30"),
+                MaximumRamMb = 4096
+            }
+        );
+
         //Lista todas as versões do mine
         var versions = await launcher.GetAllVersionsAsync();
-        foreach(var version in versions)
+        foreach (var version in versions)
         {
             Console.WriteLine(version);
         }
